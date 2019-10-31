50d6a22d2edd794c2f0dd1f1bf60e9d4ebdca3bc
Cameron Casiano <CasianoC@chc.edu>
2019-10-31 10:11:37 -0400

    Added math logic

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,11 +10,110 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var num1TextField: UITextField!
+    @IBOutlet weak var num2TextField: UITextField!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func addNums(_ sender: UIButton) {
+        var dNum1 = 0.0
+        var dNum2 = 0.0
+        var dLabel = 0.0
+        
+        if let num1 = num1TextField.text, let num2 = num2TextField.text{
+            let trimmedNum1 = num1.trimmingCharacters(in: .whitespaces)
+            let trimmedNum2 = num2.trimmingCharacters(in: .whitespaces)
+            
+            if (!trimmedNum1.isEmpty && !trimmedNum2.isEmpty){
+            
+            // convert the trimmed constants to doubles
+            dNum1 = Double(trimmedNum1)!
+            dNum2 = Double(trimmedNum2)!
+                
+                dLabel = dNum1 + dNum2
+            }
+            else{
+                outputLabel.text = "0.0"
+        }
+    }
+        outputLabel.text = (String(dLabel))
+    }
+    
+    @IBAction func subtractNums(_ sender: UIButton) {
+        var dNum1 = 0.0
+            var dNum2 = 0.0
+            var dLabel = 0.0
+            
+            if let num1 = num1TextField.text, let num2 = num2TextField.text{
+                let trimmedNum1 = num1.trimmingCharacters(in: .whitespaces)
+                let trimmedNum2 = num2.trimmingCharacters(in: .whitespaces)
+                
+                if (!trimmedNum1.isEmpty && !trimmedNum2.isEmpty){
+                
+                // convert the trimmed constants to doubles
+                dNum1 = Double(trimmedNum1)!
+                dNum2 = Double(trimmedNum2)!
+                    
+                    dLabel = dNum1 - dNum2
+            }
+                else{
+                        outputLabel.text = "0.0"
+                }
+        }
+            outputLabel.text = (String(dLabel))
+    }
+    
+    @IBAction func multiplyNums(_ sender: UIButton) {
+        var dNum1 = 0.0
+            var dNum2 = 0.0
+            var dLabel = 0.0
+            
+            if let num1 = num1TextField.text, let num2 = num2TextField.text{
+                let trimmedNum1 = num1.trimmingCharacters(in: .whitespaces)
+                let trimmedNum2 = num2.trimmingCharacters(in: .whitespaces)
+                
+                if (!trimmedNum1.isEmpty || !trimmedNum2.isEmpty){
+                
+                // convert the trimmed constants to doubles
+                dNum1 = Double(trimmedNum1)!
+                dNum2 = Double(trimmedNum2)!
+                    
+                    dLabel = dNum1 * dNum2
+            }
+                else{
+                        outputLabel.text = "0.0"
+                }
+        }
+            outputLabel.text = (String(dLabel))
+    }
+    
+    @IBAction func divideNums(_ sender: UIButton) {
+        var dNum1 = 0.0
+            var dNum2 = 0.0
+            var dLabel = 0.0
+            
+            if let num1 = num1TextField.text, let num2 = num2TextField.text{
+                let trimmedNum1 = num1.trimmingCharacters(in: .whitespaces)
+                let trimmedNum2 = num2.trimmingCharacters(in: .whitespaces)
+                
+                if (!trimmedNum1.isEmpty && !trimmedNum2.isEmpty){
+                
+                // convert the trimmed constants to doubles
+                dNum1 = Double(trimmedNum1)!
+                dNum2 = Double(trimmedNum2)!
+                    
+                    dLabel = dNum1 / dNum2
+            }
+                else{
+                        outputLabel.text = "0.0"
+                }
+        }
+            outputLabel.text = (String(dLabel))
+    }
 }
 
